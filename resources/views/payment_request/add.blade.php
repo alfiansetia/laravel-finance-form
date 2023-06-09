c42680190573fa05eee48e77e5f9e725bbdf595b
alfiansetia <alfian.setia100@gmail.com>
2023-06-09 14:48:45 +0700

    f
    fix layout form payment

--- resources/views/payment_request/add.blade.php
+++ resources/views/payment_request/add.blade.php
@@ -14,7 +14,7 @@
                         @csrf
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="id_division">Name Division</label>
+                                <label for="id_division">Name Division *</label>
                                 <select class="form-control @error('id_division') is-invalid @enderror"" id="id_division"
                                     name="id_division" required>
                                     @foreach ($division as $item)
@@ -22,10 +22,21 @@
                                     @endforeach
                                 </select>
                             </div>
+                            <div class="form-group col-md-6">
+                                <label for="beneficiary_bank">Bank *</label>
+                                <input type="text" id="beneficiary_bank" name="beneficiary_bank"
+                                    class="form-control @error('beneficiary_bank') is-invalid @enderror"
+                                    value="{{ old('beneficiary_bank') }}" required>
+                                @error('beneficiary_bank')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="invoice_date">Invoice Date</label>
+                                <label for="invoice_date">Invoice Date *</label>
                                 <input type="date" id="invoice_date" name="invoice_date"
                                     class="form-control @error('invoice_date') is-invalid @enderror"
                                     value="{{ old('invoice_date') }}" required>
@@ -36,7 +47,7 @@
                                 @enderror
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="received_date">Received Date</label>
+                                <label for="received_date">Received Date *</label>
                                 <input type="date" id="received_date" name="received_date"
                                     class="form-control @error('received_date') is-invalid @enderror"
                                     value="{{ old('received_date') }}" required>
@@ -49,18 +60,7 @@
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="contract">Contract</label>
-                                <input type="text" id="contract" name="contract"
-                                    class="form-control @error('contract') is-invalid @enderror"
-                                    value="{{ old('contract') }}">
-                                @error('contract')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="date_pr">PR Voucher Date</label>
+                                <label for="date_pr">PR Voucher Date *</label>
                                 <input type="date" id="date_pr" name="date_pr"
                                     class="form-control @error('date_pr') is-invalid @enderror"
                                     value="{{ old('date_pr') }}" required>
@@ -70,10 +70,8 @@
                                     </div>
                                 @enderror
                             </div>
-                        </div>
-                        <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="name_beneficiary">Name Beneficiary</label>
+                                <label for="name_beneficiary">Name Beneficiary *</label>
                                 <input type="text" id="name_beneficiary" name="name_beneficiary"
                                     class="form-control @error('name_beneficiary') is-invalid @enderror"
                                     value="{{ old('name_beneficiary') }}" required>
@@ -83,8 +81,10 @@
                                     </div>
                                 @enderror
                             </div>
+                        </div>
+                        <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="bank_account">Bank A/C</label>
+                                <label for="bank_account">Bank A/C *</label>
                                 <input type="text" id="bank_account" name="bank_account"
                                     class="form-control @error('bank_account') is-invalid @enderror"
                                     value="{{ old('bank_account') }}" required>
@@ -94,12 +94,32 @@
                                     </div>
                                 @enderror
                             </div>
+                            <div class="form-group col-md-6">
+                                <label for="for">For *</label>
+                                <input type="text" id="for" name="for"
+                                    class="form-control @error('for') is-invalid @enderror" value="{{ old('for') }}"
+                                    required>
+                                @error('for')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
                         </div>
                         <div class="form-row">
+                            {{-- <div class="form-group col-md-6">
+                                <label for="contract">Contract</label>
+                                <input type="text" id="contract" name="contract"
+                                    class="form-control @error('contract') is-invalid @enderror"
+                                    value="{{ old('contract') }}">
+                                @error('contract')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div> --}}
                             <div class="form-group col-md-6">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="currency">Type Currency</label>
+                                <label for="currency">Type Currency *</label>
                                 <select class="form-control @error('currency') is-invalid @enderror" id="currency"
                                     name="currency" required>
                                     <option value="idr" selected>IDR</option>
@@ -113,14 +133,13 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-row" id="add_desc_form">
                             <div class="form-group col-md-6 desc_form">
-                                <label>Description</label>
+                                <label>Description *</label>
                                 <input type="text" name="description[]" class="form-control" maxlength="120" required>
                             </div>
                             <div class="form-group col-md-6 price_form">
-                                <label>Price</label>
+                                <label>Price *</label>
                                 <input type="number" name="price[]" class="form-control" min="0" required>
                             </div>
                             <div class="form-group col-md-12" id="before">
@@ -131,38 +150,23 @@
                                     Description</a>
                             </div>
                         </div>
-
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="currency">Bank</label>
-                                <input type="text" id="beneficiary_bank" name="beneficiary_bank"
-                                    class="form-control @error('beneficiary_bank') is-invalid @enderror"
-                                    value="{{ old('beneficiary_bank') }}" required>
-                                @error('beneficiary_bank')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="for">For</label>
-                                <input type="text" id="for" name="for"
-                                    class="form-control @error('for') is-invalid @enderror" value="{{ old('for') }}"
-                                    required>
-                                @error('for')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="due_date">Due Date</label>
-                                <input type="number" id="due_date" name="due_date"
-                                    class="form-control @error('due_date') is-invalid @enderror"
-                                    value="{{ old('due_date', 0) }}" min="0">
-                                @error('due_date')
+                                <label>VAT *</label><br>
+                                <div class="form-check form-check-inline">
+                                    <label class="form-radio-label">
+                                        <input class="form-radio-input" type="radio" name="vat" value="yes"
+                                            checked>
+                                        <span class="form-radio-sign">Yes</span>
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <label class="form-radio-label">
+                                        <input class="form-radio-input" type="radio" name="vat" value="no">
+                                        <span class="form-radio-sign">No</span>
+                                    </label>
+                                </div>
+                                @error('vat')
                                     <div class="invalid-feedback">
                                         {{ $message }}
                                     </div>
@@ -186,8 +190,19 @@
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="bank_charge">Bank Charges</label>
-                                <input type="number" name="bank_charge"
+                                <label for="due_date">Due Date</label>
+                                <input type="number" id="due_date" name="due_date"
+                                    class="form-control @error('due_date') is-invalid @enderror"
+                                    value="{{ old('due_date', 0) }}" min="0">
+                                @error('due_date')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="bank_charge">Bank Charges *</label>
+                                <input type="number" id="bank_charge" name="bank_charge"
                                     class="form-control @error('bank_charge') is-invalid @enderror"
                                     value="{{ old('bank_charge', 0) }}" min="0">
                                 @error('bank_charge')
@@ -196,27 +211,7 @@
                                     </div>
                                 @enderror
                             </div>
-                            <div class="form-group col-md-6">
-                                <label>VAT</label><br>
-                                <div class="form-check form-check-inline">
-                                    <label class="form-radio-label">
-                                        <input class="form-radio-input" type="radio" name="vat" value="yes"
-                                            checked>
-                                        <span class="form-radio-sign">Yes</span>
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <label class="form-radio-label">
-                                        <input class="form-radio-input" type="radio" name="vat" value="no">
-                                        <span class="form-radio-sign">No</span>
-                                    </label>
-                                </div>
-                                @error('vat')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
+
                         </div>
                         <div class="text-right">
                             <a href="{{ route('payment.index') }}" class="btn btn-md btn-secondary ml-auto mr-2"><i
@@ -246,11 +241,11 @@
                 totalDesc++;
                 var form = $(`
                             <div class="form-group col-md-6 desc_form">
-                                <label>Description</label>
+                                <label>Description *</label>
                                 <input type="text" name="description[]" class="form-control" maxlength="120" required>
                             </div>
                             <div class="form-group col-md-6 price_form">
-                                <label>Price</label>
+                                <label>Price *</label>
                                 <input type="number" name="price[]" class="form-control" min="0" required>
                             </div>
             `);
--- resources/views/payment_request/edit.blade.php
+++ resources/views/payment_request/edit.blade.php
@@ -16,7 +16,7 @@
 
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="id_division">Name Division</label>
+                                <label for="id_division">Name Division *</label>
                                 <select class="form-control" id="id_division" name="id_division" readonly>
                                     @foreach ($division as $item)
                                         <option value="{{ $item->id }}"
@@ -25,48 +25,57 @@
                                     @endforeach
                                 </select>
                             </div>
+                            <div class="form-group col-md-6">
+                                <label for="beneficiary_bank">Bank *</label>
+                                <input type="text" id="beneficiary_bank" name="beneficiary_bank" class="form-control"
+                                    value="{{ $data->beneficiary_bank }}" required>
+                            </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="invoice_date">Invoice Date</label>
+                                <label for="invoice_date">Invoice Date *</label>
                                 <input type="date" id="invoice_date" name="invoice_date" class="form-control"
                                     value="{{ date('Y-m-d', strtotime($data->invoice_date)) }}" required>
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="received_date">Received Date</label>
+                                <label for="received_date">Received Date *</label>
                                 <input type="date" id="received_date" name="received_date" class="form-control"
                                     value="{{ date('Y-m-d', strtotime($data->received_date)) }}" required>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="contract">Contract</label>
-                                <input type="text" id="contract" name="contract" class="form-control"
-                                    value="{{ $data->contract }}">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="date_pr">PR Voucher Date</label>
+                                <label for="date_pr">PR Voucher Date *</label>
                                 <input type="date" id="date_pr" name="date_pr" class="form-control"
                                     value="{{ date('Y-m-d', strtotime($data->date_pr)) }}" readonly required>
                             </div>
-                        </div>
-                        <div class="form-row">
+
                             <div class="form-group col-md-6">
-                                <label for="name_beneficiary">Name Beneficiary</label>
+                                <label for="name_beneficiary">Name Beneficiary *</label>
                                 <input type="text" id="name_beneficiary" name="name_beneficiary" class="form-control"
                                     value="{{ $data->name_beneficiary }}" required>
                             </div>
-                            <div class="form-group col-md-6">
-                                <label for="bank_account">Bank A/C</label>
-                                <input type="text" id="bank_account" name="bank_account" class="form-control"
-                                    value="{{ $data->bank_account }}" required>
-                            </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
+                                <label for="bank_account">Bank A/C *</label>
+                                <input type="text" id="bank_account" name="bank_account" class="form-control"
+                                    value="{{ $data->bank_account }}" required>
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="currency">Type Currency</label>
+                                <label for="for">For *</label>
+                                <input type="text" id="for" name="for" class="form-control"
+                                    value="{{ $data->for }}" required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            {{-- <div class="form-group col-md-6">
+                                <label for="contract">Contract</label>
+                                <input type="text" id="contract" name="contract" class="form-control"
+                                    value="{{ $data->contract }}">
+                            </div> --}}
+                            <div class="form-group col-md-6">
+                                <label for="currency">Type Currency *</label>
                                 <select class="form-control" id="currency" name="currency" required>
                                     <option {{ $data->currency == 'idr' ? 'selected' : '' }} value="idr">IDR</option>
                                     <option {{ $data->currency == 'usd' ? 'selected' : '' }} value="usd">USD</option>
@@ -77,12 +86,12 @@
                         <div class="form-row" id="add_desc_form">
                             @foreach ($data->desc as $item)
                                 <div class="form-group col-md-6 desc_form">
-                                    <label>Description</label>
+                                    <label>Description *</label>
                                     <input type="text" name="description[]" class="form-control" maxlength="120"
                                         value="{{ $item->value }}" required>
                                 </div>
                                 <div class="form-group col-md-6 price_form">
-                                    <label>Price</label>
+                                    <label>Price *</label>
                                     <input type="number" name="price[]" class="form-control" min="0"
                                         value="{{ $item->price }}" required>
                                 </div>
@@ -96,40 +105,6 @@
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="currency">Bank</label>
-                                <input type="text" id="beneficiary_bank" name="beneficiary_bank" class="form-control"
-                                    value="{{ $data->beneficiary_bank }}" required>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="for">For</label>
-                                <input type="text" id="for" name="for" class="form-control"
-                                    value="{{ $data->for }}" required>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="due_date">Due Date</label>
-                                <input type="number" id="due_date" name="due_date" class="form-control"
-                                    min="0" value="{{ $data->due_date ?? 0 }}">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="wht">WHT</label>
-                                <select class="custom-select" id="wht" name="wht">
-                                    <option value="">Select Wht</option>
-                                    @foreach ($wht as $item)
-                                        <option {{ $data->wht_id == $item->id ? 'selected' : '' }}
-                                            value="{{ $item->id }}">{{ $item->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="bank_charge">Bank Charges</label>
-                                <input type="number" name="bank_charge" class="form-control" min="0"
-                                    value="{{ $data->bank_charge ?? 0 }}">
-                            </div>
                             <div class="form-group col-md-6">
                                 <label>VAT</label><br>
                                 <div class="form-check form-check-inline">
@@ -145,6 +120,28 @@
                                         <span class="form-radio-sign">No</span>
                                     </label>
                                 </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="wht">WHT</label>
+                                <select class="custom-select" id="wht" name="wht">
+                                    <option value="">Select Wht</option>
+                                    @foreach ($wht as $item)
+                                        <option {{ $data->wht_id == $item->id ? 'selected' : '' }}
+                                            value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="due_date">Due Date *</label>
+                                <input type="number" id="due_date" name="due_date" class="form-control"
+                                    min="0" value="{{ $data->due_date ?? 0 }}">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="bank_charge">Bank Charges *</label>
+                                <input type="number" id="bank_charge" name="bank_charge" class="form-control"
+                                    min="0" value="{{ $data->bank_charge ?? 0 }}">
                             </div>
                         </div>
                         <div class="text-right">
