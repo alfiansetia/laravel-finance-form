0a9b361eb12010c8e6f0d1169876c55003c54216
alfiansetia <alfian.setia100@gmail.com>
2023-06-12 13:26:03 +0700

    f
    add role middleware

--- app/Http/Controllers/PaymentRequestController.php
+++ app/Http/Controllers/PaymentRequestController.php
@@ -18,6 +18,7 @@ class PaymentRequestController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('role')->only(['index']);
     }
 
     public function index()
