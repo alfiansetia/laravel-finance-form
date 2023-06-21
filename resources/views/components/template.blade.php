78ea7986991352c4dc9e67fd673f46f7dbeaaa1f
alfiansetia <alfian.setia100@gmail.com>
2023-06-21 14:28:42 +0700

    f

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Tables - {{ $title }}</title>
+    <title>{{ $title }}</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="{{ asset('logo-tab.png') }}" type="image/x-icon" />
 
